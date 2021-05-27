cdc18415a9bbcf45d8f9a99b02c0cfc9b00bc1b4
federojas <frojas@itba.edu.ar>
2021-05-27 10:37:42 -0300

    algunas syscall

--- Kernel/interruptions/syscallDispatcher.c
+++ Kernel/interruptions/syscallDispatcher.c
@@ -1,15 +1,24 @@
 #include <syscallDispatcher.h>
+#include <RTC.h>
+#include <CPUFeatures.h>
 
-#define SYS_WRITE 4
+#define SYS_RTC_ID 1
+#define SYS_WRITE_ID 4
+#define SYS_CPUFEATURES_ID 2
 
 
 void syscallSelector(uint64_t sysID) {
     switch (sysID)
     {
-    case SYS_WRITE:
+    case SYS_WRITE_ID:
         //sys_Write
         break;
-    
+    case SYS_RTC_ID:
+        printCurrentTime();
+        break;
+    case SYS_CPUFEATURES_ID:
+        getCPUFeatures();
+        break;
     default:
         break;
     }
--- Kernel/libraries/RTC.c
+++ Kernel/libraries/RTC.c
@@ -2,32 +2,40 @@
 #include <naiveConsole.h>
 #include <stdint.h>
 
+#define YEAR 20 //Debe escribir aca los digitos de su año (excepto los ultimos dos)
+#define DAY_RTC_ID 0x07
+#define MONTH_RTC_ID 0x08
+#define YEAR_RTC_ID 0x09
+#define HOUR_RTC_ID 0x04
+#define MINUTE_RTC_ID 0x02
+#define SECOND_RTC_ID 0x00
+
 void printCurrentTime()
 {
 	ncPrint("Fecha de hoy: "); //DESPUES REEMPLAZAR POR NUESTRO PRINT CON DRIVER DE VIDEO
-	uint8_t x = _getRTCInfo(0x07);
+	uint8_t x = _getRTCInfo(DAY_RTC_ID);
 	uint8_t result = ((x / 16) * 10) + (x & 0xf);
 	ncPrintDec(result); //DESPUES REEMPLAZAR POR NUESTRO PRINT CON DRIVER DE VIDEO
 	ncPrint("/");
 
-	x = _getRTCInfo(0x08);
+	x = _getRTCInfo(MONTH_RTC_ID);
 	result = ((x / 16) * 10) + (x & 0xf);
 	ncPrintDec(result);
 	ncPrint("/");
 
-	x = _getRTCInfo(0x09);
+	x = _getRTCInfo(YEAR_RTC_ID);
 	result = ((x / 16) * 10) + (x & 0xf);
-	ncPrintDec(20);
+	ncPrintDec(YEAR);
 	ncPrintDec(result);
 	ncNewline();
 
 	ncPrint("Hora: ");
-	x = _getRTCInfo(0x04);
+	x = _getRTCInfo(HOUR_RTC_ID);
 	result = ((x / 16) * 10) + (x & 0xf);
 	ncPrintDec(result);
 	ncPrint(":");
 
-	x = _getRTCInfo(0x02);
+	x = _getRTCInfo(MINUTE_RTC_ID);
 	result = ((x / 16) * 10) + (x & 0xf);
 	if(result < 10){
 		ncPrintDec(0);
@@ -35,7 +43,7 @@ void printCurrentTime()
 	ncPrintDec(result);
 	ncPrint(":"); 
 
-	x = _getRTCInfo(0x00);
+	x = _getRTCInfo(SECOND_RTC_ID);
 	result = ((x / 16) * 10) + (x & 0xf);
 		if(result < 10){
 		ncPrintDec(0);
